a7d2f8d0a8606fc6637ce19d4a578a3b48543e11
jpic <jamespic@gmail.com>
2013-02-15 04:40:25 +0100

    Rewrote script

--- session_security/static/session_security/script.js
+++ session_security/static/session_security/script.js
@@ -1,134 +1,84 @@
-// Simple function that adds a number of seconds to a Date object.
-//
-// Example usage, to add two seconds to a date:
-//
-//     d = new Date();
-//     addSeconds(d, 120);
-function addSeconds(date, seconds) {
-    var sum = date.getSeconds() + seconds;
-    date.setSeconds(sum);
-    return date;
-}
+// Use 'yourlabs' as namespace.
+if (window.yourlabs == undefined) window.yourlabs = {};
 
-// An **global instance** of `SessionSecurity` is instanciated as such by the
-// default setup (all.html):
-//
-//     sessionSecurity = new SessionSecurity();
-//     sessionSecurity = $.extend(SessionSecurity, {
-//         // define overrides here
-//     });
-//     sessionSecurity.initialize()
-var SessionSecurity = function() {
+// Session security class.
+yourlabs.SessionSecurity = function(pingUrl) {
+    // Url to PingView.
+    this.pingUrl = pingUrl;
+
     // **HTML element** that should show to warn the user that his session will
     // expire.
-    this.warningElement = $('#session_security_warning');
+    this.$warning = $('#session_security_warning');
 
-    // **Callback** for when session expires.
-    this.expire = function() {
-        $.get(this.LOGOUT_URL, function() {
-            var url = sessionSecurity.LOGIN_URL + '?next=' + document.location.pathname;
-            $('body').html('Your session has expired. <a href="' + url + '">Login again</a>');
-        });
-    }
+    // **HTML element** that should replace <body> contents when the session
+    // has expired, hiding potentially sensible data from HTML source code.
+    this.$expired = $('#session_security_expired');
 
-    // **Callback** that should display the warning.
-    this.warn = function() {
-        this.warningElement.fadeIn();
-    }
+    // A hack to anticipate clock skews. If the next event (warn or expire) is
+    // in 13 seconds and that timeRatio is 1.3, then it will hit PingView after
+    // 10 seconds (10/1.3). Adjust to your needs when you are asked to fine-tune.
+    this.timeRatio = 1.3;
 
-    // **Callback** for activity events, mouse move, keyboard move, scroll ...
-    // 
-    // **Beware**: it is bind as function, which means that `this` will **not**
-    // refer to the SessionSecurity instance. Use `sessionSecurity` global
-    // variable instead of `this`.
-    this.activity = function() {
-        sessionSecurity.lastActivity = new Date();
+    // Last recorded activity datetime.
+    this.lastActivity = new Date();
 
-        if (sessionSecurity.warningElement.is(':visible')) {
-            sessionSecurity.warningElement.hide();
+    // Bind common activity events to update this.lastActivity.
+    $(document)
+        .scroll($.proxy(this.activity, this))
+        .keyup($.proxy(this.activity, this))
+        .mousemove($.proxy(this.activity, this))
+        .click($.proxy(this.activity, this))
 
-            // Cancel the next programed tick.
-            clearTimeout(sessionSecurity.timeout);
-            // Tick now, to upload last activity time to the server.
-            sessionSecurity.tick(true);
-        }
-    }
-
-    this.tick = function(activity) {
-        var now = new Date();
-        if (activity) {
-            // Was called from `activity()`, update the server's
-            // `last_activity`.
-            var sinceActivity = Math.floor((now - sessionSecurity.lastActivity)
-                / 1000);
-        } else {
-            // Was called to warn or expire, don't update the server.
-            var sinceActivity = -1;
-        }
-
-        $.get(
-            sessionSecurity.pingUrl, 
-            {
-                'sinceActivity': sinceActivity,
-                'csrfmiddlewaretoken': sessionSecurity.token,
-            },
-            function(data, textStatus, jqXHR) {
-                // If `sinceActivity` is different between the server and the
-                // client:
-                if (sinceActivity != parseInt(data)) {
-                    var sinceActivity = parseInt(data);
-                    sessionSecurity.lastActivity = addSeconds(new Date(), sinceActivity * -1);
-                }
-
-                // Pre-calculate in how many seconds to warn or expire using
-                // `sinceActivity` which might have been updated (see above).
-                expireIn = sessionSecurity.EXPIRE_AFTER - sinceActivity;
-                warnIn = sessionSecurity.WARN_AFTER - sinceActivity;
-
-                if (expireIn <= 0) {
-                    // No time left before expiration.
-                    sessionSecurity.expire();
-                } else if (warnIn <= 0) {
-                    // No time left before warning.
-                    sessionSecurity.warn();
-
-                    // As time goes by, we want to poll the server for last
-                    // activity updates (maybe done in another browser tab)
-                    // more often:
-                    //
-                    // - If the session should
-                    //   **expire in 5 seconds**, then we want to **poll in 2**,
-                    // - If the session should **expire in 2 seconds**, then we
-                    //   want to **poll in 1**.
-                    var next = expireIn / 2;
-
-                    next = next < 1 ? 1 : next;
-
-                    sessionSecurity.timeout = setTimeout(sessionSecurity.tick, 
-                        next * 1000);
-                } else {
-                    // Apparently there is still time, tick at the next
-                    // expected warning time, before displaying the warning.
-                    sessionSecurity.warningElement.hide();
-                    sessionSecurity.timeout = setTimeout(sessionSecurity.tick, 
-                        warnIn * 1000);
-                }
-            }
-        );
-    }
-
-    this.initialize = function() {
-        // Initiate `this.lastActivity`.
-        this.lastActivity = new Date();
-
-        // Try to monitor for activity in the page.
-        $(document).scroll(this.activity);
-        $(document).keyup(this.activity);
-        $(document).mousemove(this.activity);
-        $(document).click(this.activity);
-
-        // Tick when warning time is expected.
-        this.timeout = setTimeout(sessionSecurity.tick, this.WARN_AFTER*1000);
-    }
+    // Ping to get the next event type and in how many seconds.
+    this.ping();
 }
 
+yourlabs.SessionSecurity.prototype = {
+    // Called when PingView responds with ['expire', <something lower than 0>].
+    expire: function() {
+        $('body').html(this.$expired);
+        this.expired = true;
+    },
+    
+    // Called when PingView responds with 
+    // ['expire', <something higher than 0>].
+    showWarning: function() {
+        this.$warning.fadeIn('slow');
+    },
+    
+    // Called when PingView responds with ['warn', ...]
+    hideWarning: function() {
+        this.$warning.hide();
+    },
+
+    // Called by click, scroll, mousemove, keyup.
+    activity: function() {
+        this.hideWarning();
+        this.lastActivity = new Date();
+    },
+
+    // Hit the PingView with the number of seconds since last activity.
+    ping: function() {
+        var inactiveSince = Math.floor((new Date() - this.lastActivity)
+            / 1000);
+
+        $.post(this.pingUrl, {inactiveSince: inactiveSince},
+           $.proxy(this.pong, this), 'json');
+    },
+
+    // Callback to process PingView response.
+    pong: function(data) {
+        if (this.expired) return;
+
+        this.action = data[0];
+        this.time = data[1];        
+
+        if (this.action == 'expire') {
+            this.time <= 0 ? this.expire() : this.showWarning();
+        } else {
+            this.hideWarning();
+        }
+        this.timeout = setTimeout($.proxy(this.ping, this), 
+            (this.time / this.timeRatio) * 1000);
+    },
+}
