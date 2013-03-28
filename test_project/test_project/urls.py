9e7356821a61199b98d56890dac9cc227033fcd1
jpic <jamespic@gmail.com>
2013-03-28 17:38:47 +0100

    Coded the use case described in #1

--- test_project/test_project/urls.py
+++ test_project/test_project/urls.py
@@ -1,18 +1,24 @@
+import time
+
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
+from django.contrib.auth.decorators import login_required
+from django.views import generic
+
+
+class SleepView(generic.TemplateView):
+    def get(self, request, *args, **kwargs):
+        time.sleep(int(request.GET.get('seconds', 0)))
+        return super(SleepView, self).get(request, *args, **kwargs)
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'test_project.views.home', name='home'),
-    # url(r'^test_project/', include('test_project.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    url(r'^$', generic.TemplateView.as_view(template_name='home.html')),
+    url(r'^sleep/$', login_required(
+        SleepView.as_view(template_name='home.html')), name='sleep'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'session_security/', include('session_security.urls')),
 )
