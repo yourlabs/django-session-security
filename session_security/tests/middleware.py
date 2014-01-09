de82e2d359f6c8bb7e36329fa63e9748aefbac4f
Aaron Krill <aaron@krillr.com>
2014-01-09 06:32:43 -0800

    Add test for when last logged activity is in future

--- session_security/tests/middleware.py
+++ session_security/tests/middleware.py
@@ -2,6 +2,8 @@ import time
 import unittest
 
 from django.test.client import Client
+from session_security.utils import set_last_activity
+from datetime import datetime, timedelta
 
 
 class MiddlewareTestCase(unittest.TestCase):
@@ -15,6 +17,14 @@ class MiddlewareTestCase(unittest.TestCase):
         time.sleep(12)
         response = self.client.get('/admin/')
         self.assertFalse('_auth_user_id' in self.client.session)
+
+    def test_last_activity_in_future(self):
+        self.client.login(username='test', password='test')
+        now = datetime.now()
+        future = now + timedelta(0, 30)
+        set_last_activity(self.client.session, future)
+        response = self.client.get('/admin/')
+        self.assertTrue('_auth_user_id' in self.client.session)
 
     def test_non_javascript_browse_no_logout(self):
         self.client.login(username='test', password='test')
