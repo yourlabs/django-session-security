d8ec759f528cb2c1747307094b6e50f535810cc0
jpic <jamespic@gmail.com>
2014-05-16 00:09:07 +0200

    Pre-compile .mo.
    https://github.com/yourlabs/django-autocomplete-light/pull/280

--- setup.py
+++ setup.py
@@ -34,25 +34,10 @@ class RunTests(Command):
         os.chdir(this_dir)
 
 if 'sdist' in sys.argv:
-    # clear compiled mo files before building the distribution
-    walk = os.walk(os.path.join(os.getcwd(), 'session_security/locale'))
-    for dirpath, dirnames, filenames in walk:
-        if not filenames:
-            continue
-
-        if 'django.mo' in filenames:
-            os.unlink(os.path.join(dirpath, 'django.mo'))
-else:
-    # if django is there, compile the po files to mo,
-    try:
-        import django
-    except ImportError:
-        pass
-    else:
-        dir = os.getcwd()
-        os.chdir(os.path.join(dir, 'session_security'))
-        os.system('django-admin.py compilemessages')
-        os.chdir(dir)
+    dir = os.getcwd()
+    os.chdir(os.path.join(dir, 'session_security'))
+    os.system('django-admin.py compilemessages')
+    os.chdir(dir)
 
 
 setup(
