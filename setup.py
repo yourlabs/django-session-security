cea64bfd2686d964cb71f0f1ce7d8c946f75c117
jpic <jamespic@gmail.com>
2014-05-16 00:07:47 +0200

    Version bump 2.1.7

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ else:
 
 setup(
     name='django-session-security',
-    version='2.1.6',
+    version='2.1.7',
     description='Let the user secure his session for usage in public computers',
     author='James Pic',
     author_email='jamespic@gmail.com',
