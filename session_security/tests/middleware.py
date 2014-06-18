eeaed988e3012a3b008478b3631e89a7d3d7fba6
Vuong Nguyen <v@ncryptedcloud.com>
2014-06-18 00:27:41 -0400

    Monkey patching the custom settings-Middleware tests now passed

--- session_security/tests/middleware.py
+++ session_security/tests/middleware.py
@@ -4,9 +4,9 @@ from datetime import datetime, timedelta
 
 from django.test import TestCase as DjangoTestCase
 from django.test.client import Client
-from django.test.utils import override_settings
 
 from session_security.utils import set_last_activity
+from session_security import settings
 
 
 class MiddlewareTestCase(unittest.TestCase):
@@ -50,15 +50,27 @@ class MiddlewareTestCase(unittest.TestCase):
         self.assertTrue('_auth_user_id' in self.client.session)
 
 
-@override_settings(
-    SESSION_SECURITY_EXPIRE_AFTER=3,
-    SESSION_SECURITY_WARN_AFTER=2,
-    SESSION_SECURITY_WARN_BEFORE=1,
-    SESSION_SECURITY_CUSTOM_SESSION_KEY='user-auto-logout')
 class DynamicSessionLevelTestCase(DjangoTestCase):
+    def monkey_patch(self, saved_settings=None):
+        values = {'EXPIRE_AFTER': 3,
+                  'WARN_BEFORE': 2,
+                  'WARN_AFTER': 1,
+                  'EXPIRE_AFTER_CUSTOM_SESSION_KEY': 'user-session-key'}
+        old_settings = {}
+        for k, v in values.items():
+            old_settings[k] = getattr(settings, k, None)
+            setattr(
+                settings, k,
+                v if saved_settings is None else saved_settings.get(k))
+        return old_settings
+
     def setUp(self):
         self.client = Client()
         self.client.login(username='test', password='test')
+        self.saved_settings = self.monkey_patch()
+
+    def tearDown(self):
+        self.monkey_patch(saved_settings=self.saved_settings)
 
     def set_custom_expire_after_value(self, value):
         s = self.client.session
