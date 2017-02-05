1622459a733f0179d20d4d5d1e70a57b5ab7bc3d
jpic <jamespic@gmail.com>
2017-02-05 15:06:57 +0100

    Release 2.4.1

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ if 'sdist' in sys.argv:
 
 setup(
     name='django-session-security',
-    version='2.4.0',
+    version='2.4.1',
     description='Let the user secure his session for usage in public computers',
     author='James Pic',
     author_email='jamespic@gmail.com',
