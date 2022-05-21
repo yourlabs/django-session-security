fe9188a2791eebbb2cd5ef136b99d57cc3b3eaa0
Michał Pasternak <michal.dtz@gmail.com>
2022-05-21 05:31:04 +0200

    switch_to_window -> switch_to.window

--- session_security/tests/test_script.py
+++ session_security/tests/test_script.py
@@ -19,7 +19,7 @@ class ScriptTestCase(BaseLiveServerTestCase):
         start = datetime.datetime.now()
 
         for win in self.sel.window_handles:
-            self.sel.switch_to_window(win)
+            self.sel.switch_to.window(win)
             try:
                 el = WebDriverWait(self.sel, self.max_warn_after).until(
                 expected_conditions.visibility_of_element_located((By.ID, "session_security_warning")))
@@ -33,7 +33,7 @@ class ScriptTestCase(BaseLiveServerTestCase):
         self.assertLessEqual(delta.seconds, self.max_warn_after)
 
         for win in self.sel.window_handles:
-            self.sel.switch_to_window(win)
+            self.sel.switch_to.window(win)
             try:
                 el = WebDriverWait(self.sel, self.max_expire_after).until(
                 expected_conditions.visibility_of_element_located((By.ID, "id_password")))
@@ -53,7 +53,7 @@ class ScriptTestCase(BaseLiveServerTestCase):
             self.press_space()
 
             for win in self.sel.window_handles:
-                self.sel.switch_to_window(win)
+                self.sel.switch_to.window(win)
 
             try:
                 el = WebDriverWait(self.sel, 20).until(
@@ -77,7 +77,7 @@ class ScriptTestCase(BaseLiveServerTestCase):
             assert(el.is_displayed())
 
             for win in self.sel.window_handles:
-                self.sel.switch_to_window(win)
+                self.sel.switch_to.window(win)
 
             delta = datetime.datetime.now() - start
             self.assertGreaterEqual(delta.seconds, self.min_warn_after)
